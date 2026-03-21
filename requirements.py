cdc99e444bca98c038338f05fd61f63351cfe602
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-03-21 22:49:38 +0800

    Add required packages to requirements.py

---
+++ requirements.py
@@ -0,0 +1,11 @@
+# কোর প্যাকেজ
+python-telegram-bot==21.0.1
+pandas>=2.3.3
+python-dotenv==1.0.0
+huggingface-hub==0.20.3
+# অতিরিক্ত প্রয়োজনীয় প্যাকেজ
+requests>=2.31.0
+groq
+flask==2.3.3
+google-genai 
+numpy
